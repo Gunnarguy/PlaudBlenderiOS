5d1a78994db43ed08ca4ae5a5f3f7ca6a06575af
gunnarguy <gunnarguy@me.com>
2026-03-21 19:10:37 -0700

    Initial Commit

---
+++ PlaudBlenderiOS/PlaudBlenderiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlaudBlenderiOSApp.swift
+//  PlaudBlenderiOS
+//
+//  Created by Gunnar Hostetler on 3/21/26.
+//
+
+import SwiftUI
+
+@main
+struct PlaudBlenderiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
